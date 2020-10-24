b67fbbaefe4442190a00fe6c6611aca7f9fff83b
gogoend <gogoend@qq.com>
2020-10-24 17:04:35 +0800

    enhance: 替换原先的服务器脚本

--- server/index.js
+++ server/index.js
@@ -1,23 +1,32 @@
 // nodejs服务器
 const express =require('express')
-const Vue = require('vue')
-
+const fs = require('fs')
 const app = express()
 
-const renderer = require('vue-server-renderer').createRenderer()
+const {createBundleRenderer} = require('vue-server-renderer')
 
-const page = new Vue({
-    data:{
-        title:'gogoend'
-    },
-    template: '<h1>Hello, {{title}}</h1>'
+const serverBundle = require('../dist/server/vue-ssr-server-bundle.json');
+const clientManifest = require('../dist/client/vue-ssr-client-manifest.json')
+const renderer = createBundleRenderer(serverBundle,{
+    runInNewContext: false,
+    template: fs.readFileSync(__dirname+'\\..\\public\\index.temp.html','utf-8'),// 宿主模板
+    clientManifest
 })
 
-app.get('/', async (req,res)=>{
+app.use(express.static(__dirname+'\\..\\dist\\client',{
+    index:false
+}))
+
+// '/' 改为 '*' 用于把路由交给SSR进行接管
+app.get('*', async (req,res)=>{
     // 最好进行异常捕获
     try{
+        const context = {
+            url: req.url,
+            title: 'ssr test'
+        }
         // 用渲染器渲染page可以得到html内容
-        const html = await renderer.renderToString(page)
+        const html = await renderer.renderToString(context)
         console.log(html)
         res.send(html)
     }catch(err){
