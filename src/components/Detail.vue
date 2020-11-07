ad801003e4d53a52fc837c88210a915272795b21
gogoend <gogoend@qq.com>
2020-11-07 17:40:28 +0800

    enhance: 文案更新

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,13 +1,16 @@
 <template>
-  <h1>Detail</h1>
+  <div>
+    <h1>Detail</h1>
+    <p>锄禾日当午</p>
+    <p>啥都不靠谱</p>
+    <p>闲来没事做</p>
+    <p>不如斗地主</p>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
